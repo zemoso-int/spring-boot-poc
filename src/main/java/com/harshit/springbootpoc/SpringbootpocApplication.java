8365ba784f7614eab1a896dcb8564177a8e53076
harshit-vidhyarthi <harshitvidhyarthi@gmail.com>
2022-04-14 08:13:22 +0530

    project setup

---
+++ src/main/java/com/harshit/springbootpoc/SpringbootpocApplication.java
@@ -0,0 +1,13 @@
+package com.harshit.springbootpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootpocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harshit/springbootpoc/SpringbootpocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshit.springbootpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootpocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
